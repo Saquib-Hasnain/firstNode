415168ee0b87023ab53ee854f78cf7fd382fa968
Saquib Hasnain <101635364+Saquib-Hasnain@users.noreply.github.com>
2023-12-31 00:26:26 +0530

    Request / Response
    return a response like "Welcome to my Node Js project". Just follow the way he writes code in 30th video.
    Based on the url the user hits , I want you to return custom responses.
    When url = /home , return response ==> Welcome home
    When url = /about, return response ==> Welcome to About Us page
    When url =/node, return response ==> Welcome to my Node Js project

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
-    console.log("Saquib Hasnain")
+    console.log(req.url,req.method,req.headers)
+   // process.exit()
+   res.setHeader('Content-Type','text/html');
+   res.write('<html>')
+   res.write('<head><title>My first Page</title></head>')
+   res.write('<body><h1>Welcome to my Node Js project</h1><a href="/home">Home</a><br><a href="/about">About</a><br><a href="/node">Node</a></body>')
+   if (req.url === '/home') {
+    res.write('<html><head><title>Welcome Home</title></head><body><h1>Welcome home</h1></body></html>');
+} else if (req.url === '/about') {
+    res.write('<html><head><title>About Us</title></head><body><h1>Welcome to About Us page</h1></body></html>');
+} else if (req.url === '/node') {
+    res.write('<html><head><title>Node Js Project</title></head><body><h1>Welcome to my Node Js project</h1></body></html>');
+} else {
+    // Default response for other paths
+    res.write('<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1></body></html>');
+}
+   res.write('</html>')
+   res.end()
+   
 })
-server.listen(4000)
+server.listen(3000)
